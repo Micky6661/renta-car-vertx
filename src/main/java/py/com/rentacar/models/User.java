014eb35c6256acd8f6f5cf6770e2862fe92409f9
Miguel Martinez <mickygates27@gmail.com>
2018-09-05 14:44:15 -0400

    Agregada Conexion a BD Hibernate

--- src/main/java/py/com/rentacar/models/User.java
+++ src/main/java/py/com/rentacar/models/User.java
@@ -1,35 +1,48 @@
 package py.com.rentacar.models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
+/**
+ * @author Miguel Martinez
+ **/
+@Entity
+@Table(name = "User")
 public class User implements Serializable {
-	private String username;
-	private String password;
+    @Id
+    private Integer userId;
 
-	private static final long serialVersionUID = -8672858398542565036L;
+    private String username;
 
-	public User() {
-	}
+    private String password;
 
-	public User(String username, String password) {
-		this.username = username;
-		this.password = password;
-	}
+    private static final long serialVersionUID = -8672858398542565036L;
 
-	public String getUsername() {
-		return username;
-	}
+    public User() {
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
